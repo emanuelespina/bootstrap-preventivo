91f9cac2442e7af41e09d4f5786713da58cd1192
emanuelespina <spina.emanuele98@gmail.com>
2025-03-14 01:04:17 +0100

    fead: add select alert

--- js/script.js
+++ js/script.js
@@ -9,15 +9,17 @@ const emailUser = document.getElementById("emailInput");
 
 const workSelect = document.getElementById("workSelect");
 
+const selecAlert = document.getElementById("selecAlert");
+
 const floatingTextarea = document.getElementById("floatingTextarea");
 
 const promotionalCode = document.getElementById("promotionalCode");
 
-const promotionalAllert = document.getElementById("promotionalAllert");
+const promotionalAlert = document.getElementById("promotionalAlert");
 
 const privacyCheck = document.getElementById("privacyCheck");
 
-const checkAllert = document.getElementById("checkAllert")
+const checkAlert = document.getElementById("checkAlert")
 
 const finalPrice = document.getElementById("finalPrice");
 
@@ -38,7 +40,7 @@ const works = [
     },
 ];
 
-const discountsCode = ["YHDNU32","JANJC63","PWKCN25","SJDPO96","POCIE24"];
+const discountsCode = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
 const discount = 25;
 
@@ -48,9 +50,9 @@ const workhours = 10;
 
 //Creazione in modo dinamico della parte select
 
-for (let i = 0; i < works.length; i++){
+for (let i = 0; i < works.length; i++) {
     workSelect.innerHTML += `<option value="${i}">${works[i].work}</option>`;
-        
+
 }
 
 ////////////////////////////////////////////////
@@ -65,74 +67,80 @@ preventiveForm.addEventListener("submit", preventiveCalculator);
 
 // Fuozione principale che si avvierà se premiamo il pulsante
 
-function preventiveCalculator (event) {
+function preventiveCalculator(event) {
 
     event.preventDefault();
 
     //setto gli allarmi non visibili
 
-    checkAllert.classList.add("d-none");
+    checkAlert.classList.add("d-none");
 
-    promotionalAllert.classList.add("d-none");
+    promotionalAlert.classList.add("d-none");
+
+    selecAlert.classList.add("d-none");    
 
     const workPrice = workhours * works[workSelect.value].price;
+    
 
-    console.log(workPrice);
+    if (privacyCheck.checked === true) {
 
-    if (privacyCheck.checked === true){
+        if (workSelect.value === "null") {
+            selecAlert.classList.remove("d-none");
+        } else {
 
-        if (promotionalCode.value === ""){
-            finalPrice.innerHTML = graficPrice(workPrice.toFixed(2));
-        }else if (correctDiscountCode(promotionalCode.value) === true){
-            finalPrice.innerHTML = graficPrice((workPrice - (workPrice * discount)/100).toFixed(2));
-        }else {
-            finalPrice.innerHTML = graficPrice(workPrice.toFixed(2));
-            promotionalAllert.classList.remove("d-none");
-        }      
+            if (promotionalCode.value === "") {
+                finalPrice.innerHTML = graficPrice(workPrice.toFixed(2));
+            } else if (correctDiscountCode(promotionalCode.value) === true) {
+                finalPrice.innerHTML = graficPrice((workPrice - (workPrice * discount) / 100).toFixed(2));
+            } else {
+                finalPrice.innerHTML = graficPrice(workPrice.toFixed(2));
+                promotionalAlert.classList.remove("d-none");
+            }
+        }
 
     } else {
-        checkAllert.classList.remove("d-none");
-    }            
-        
-}
+        checkAlert.classList.remove("d-none");
+    }
+
+};
 
 // Funzione per verificare che il codice promozionale sia corretto
 
-function correctDiscountCode (code){
+function correctDiscountCode(code) {
 
     let correctCode = false;
 
-    for (let i = 0; i < discountsCode.length; i++){
-        if(code === discountsCode[i]){
+    for (let i = 0; i < discountsCode.length; i++) {
+        if (code === discountsCode[i]) {
             correctCode = true
         }
     }
 
     return correctCode;
-    
-}
+
+};
 
 // Funzione per immettere il prezzo finale con la grafica corretta
 
-function graficPrice (price){
+function graficPrice(price) {
 
-    let point = price.indexOf("."); 
-        
-    let boltNumber = "";       
+    let point = price.indexOf(".");
+
+    let boltNumber = "";
 
     let normalNumber = "";
 
-    for (let i = 0; i < price.length; i++){
+    for (let i = 0; i < price.length; i++) {
 
-        if (i < point){
+        if (i < point) {
             boltNumber += price[i];
-        }else if (i > point) {
+        } else if (i > point) {
             normalNumber += price[i];
         }
 
-    }   
+    }
 
     return `<span class="h4">€ ${boltNumber}</span>,${normalNumber}`;
 
-}
+};
 
